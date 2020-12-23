179e478a1ef4362c19bffd031c14121975bece31
Dolphin <dolphin.orca@gmail.com>
2020-12-23 15:24:48 +0900

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-    value = "${aws_vpc.lambdas.id}"
+  value = "${aws_vpc.lambdas.id}"
 }
 
 output "private_subnet_id" {
-    value = "${aws_subnet.lambdas-private.id}"
+  value = "${aws_subnet.lambdas-private.id}"
 }
 
 output "eip_id" {
-    value = "${aws_eip.lambdas.id}"
+  value = "${aws_eip.lambdas.id}"
 }
 
 output "security_group_id" {
-    value = aws_vpc.lambdas.default_security_group_id
+  value = aws_vpc.lambdas.default_security_group_id
 }
